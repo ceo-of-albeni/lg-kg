442a66bde4eecdab1f2069d2972593e3e6cc92a4
ceo-of-albeni <aliiamalaeva@gmail.com>
2025-06-02 16:24:23 +0600

    allow cors

--- app/lg_backend_api/settings.py
+++ app/lg_backend_api/settings.py
@@ -70,7 +70,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,6 +82,7 @@ MIDDLEWARE = [
 ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(" ")
 
 ROOT_URLCONF = 'lg_backend_api.urls'
 
